e6a94970a5fb28e87ded857d96c667a24f756719
Jen Herting <jen@herting.cc>
2020-05-13 15:51:52 -0400

    [g-mmpbsa] flake8 and fixme removal

--- var/spack/repos/builtin/packages/g-mmpbsa/package.py
+++ var/spack/repos/builtin/packages/g-mmpbsa/package.py
@@ -3,23 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install g-mmpbsa
-#
-# You can edit this file again by typing:
-#
-#     spack edit g-mmpbsa
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
@@ -32,26 +15,15 @@ class GMmpbsa(CMakePackage):
     homepage = "https://rashmikumari.github.io/g_mmpbsa/"
     git      = "https://github.com/RashmiKumari/g_mmpbsa"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
-    # FIXME: Add proper versions and checksums here.
-    # version('1.2.3', '0123456789abcdef0123456789abcdef')
     version('master', branch='master')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
     depends_on('gromacs@4.5:4.6.999,5.0:5.1.999~double_precision~cuda~mpi~tng simd=none')
     depends_on('apbs')
 
     def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
         spec = self.spec
         args = []
-        
+
         args.append('-DGMX_PATH={0}'.format(spec['gromacs'].prefix))
         args.append('-DAPBS_INSTALL={0}'.format(spec['apbs'].prefix))
         args.append('-DAPBS_SRC={0}'.format(spec['apbs'].prefix.src))
